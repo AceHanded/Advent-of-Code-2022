c69342b8668c0b63ea745c314fd96303448ae08b
Vili Könönen <119615259+AceHanded@users.noreply.github.com>
2022-12-04 10:52:11 +0200

    Add files via upload

--- Puzzles/Day 3/day3.py
+++ Puzzles/Day 3/day3.py
@@ -8,7 +8,24 @@ CHARS = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "
          "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 
-def read_file(gold=None):
+def silver():
+    total_priority = 0
+
+    with open("day3_input.txt", mode="r") as input_file:
+        for line in input_file:
+            line = line.rstrip()
+
+            first_half, second_half = line[:len(line) // 2], line[len(line) // 2:]
+
+            for character in first_half:
+                if character in [*second_half]:
+                    total_priority += CHARS.index(character) + 1
+                    break
+
+        return total_priority
+
+
+def gold():
     total_priority = 0
     lines = []
 
@@ -16,24 +33,14 @@ def read_file(gold=None):
         for line in input_file:
             line = line.rstrip()
 
-            if gold:
-                if len(lines) != 3:
-                    lines.append(line)
-                else:
-                    total_priority += find_common_character(lines)
-
-                    lines = [line]
-
+            if len(lines) != 3:
+                lines.append(line)
             else:
-                first_half, second_half = line[:len(line) // 2], line[len(line) // 2:]
+                total_priority += find_common_character(lines)
 
-                for character in first_half:
-                    if character in [*second_half]:
-                        total_priority += CHARS.index(character) + 1
-                        break
+                lines = [line]
 
-        if gold:
-            total_priority += find_common_character(lines)
+        total_priority += find_common_character(lines)
 
         return total_priority
 
@@ -41,20 +48,19 @@ def read_file(gold=None):
 def find_common_character(lines):
     priority = 0
 
-    for line_ in lines:
-        for character in line_:
+    for line in lines:
+        for character in line:
             if character in lines[0] and character in lines[1] and character in lines[2]:
                 priority = CHARS.index(character) + 1
                 break
-        break
 
     return priority
 
 
 def main():
-    silver = read_file()
-    gold = read_file(gold=True)
-    print(f"Silver: {silver}\nGold: {gold}")
+    silver_ = silver()
+    gold_ = gold()
+    print(f"Silver: {silver_}\nGold: {gold_}")
 
 
 if __name__ == "__main__":
