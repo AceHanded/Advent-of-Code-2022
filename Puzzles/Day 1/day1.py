732f9295a32c29ed32469268e238aabd3d487d7b
Vili Könönen <119615259+AceHanded@users.noreply.github.com>
2022-12-04 10:50:37 +0200

    Add files via upload

--- Puzzles/Day 1/day1.py
+++ Puzzles/Day 1/day1.py
@@ -3,7 +3,24 @@ Day 1 puzzles of the Advent of Code.
 """
 
 
-def read_file(gold=None):
+def silver():
+    calories = []
+    total_calories = 0
+
+    with open("day1_input.txt", mode="r") as input_file:
+        for line in input_file:
+            if line != "\n":
+                total_calories += int(line)
+            else:
+                calories.append(total_calories)
+                total_calories = 0
+
+    max_calories = max(calories)
+
+    return max_calories
+
+
+def gold():
     calories = []
     total_calories = 0
     max_calories = 0
@@ -16,21 +33,17 @@ def read_file(gold=None):
                 calories.append(total_calories)
                 total_calories = 0
 
-    if gold:
-        for value in range(0, 3):
-            max_calories += max(calories)
-            calories.pop(calories.index(max(calories)))
-
-    else:
-        max_calories = max(calories)
+    for value in range(0, 3):
+        max_calories += max(calories)
+        calories.pop(calories.index(max(calories)))
 
     return max_calories
 
 
 def main():
-    silver = read_file()
-    gold = read_file(gold=True)
-    print(f"Silver: {silver}\nGold: {gold}")
+    silver_ = silver()
+    gold_ = gold()
+    print(f"Silver: {silver_}\nGold: {gold_}")
 
 
 if __name__ == "__main__":
