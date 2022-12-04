b299290b7d52b39cfae013e65af31a7e9a2eec33
Vili Könönen <119615259+AceHanded@users.noreply.github.com>
2022-12-04 10:51:21 +0200

    Add files via upload

--- Puzzles/Day 2/day2.py
+++ Puzzles/Day 2/day2.py
@@ -8,17 +8,11 @@ DRAW = 3
 WIN = 6
 
 
-def read_file(gold=None):
+def silver():
     total_points = 0
-
-    if gold:
-        moves = {"A": {"X": LOSS+3, "Y": DRAW+1, "Z": WIN+2},
-                 "B": {"X": LOSS+1, "Y": DRAW+2, "Z": WIN+3},
-                 "C": {"X": LOSS+2, "Y": DRAW+3, "Z": WIN+1}}
-    else:
-        moves = {"A": {"X": DRAW+1, "Y": WIN+2,  "Z": LOSS+3},
-                 "B": {"X": LOSS+1, "Y": DRAW+2, "Z": WIN+3},
-                 "C": {"X": WIN+1,  "Y": LOSS+2, "Z": DRAW+3}}
+    moves = {"A": {"X": DRAW+1, "Y": WIN+2,  "Z": LOSS+3},
+             "B": {"X": LOSS+1, "Y": DRAW+2, "Z": WIN+3},
+             "C": {"X": WIN+1,  "Y": LOSS+2, "Z": DRAW+3}}
 
     with open("day2_input.txt", mode="r") as input_file:
         for line in input_file:
@@ -27,13 +21,29 @@ def read_file(gold=None):
 
             total_points += moves[opponent][you]
 
-        return total_points
+    return total_points
+
+
+def gold():
+    total_points = 0
+    moves = {"A": {"X": LOSS + 3, "Y": DRAW + 1, "Z": WIN + 2},
+             "B": {"X": LOSS + 1, "Y": DRAW + 2, "Z": WIN + 3},
+             "C": {"X": LOSS + 2, "Y": DRAW + 3, "Z": WIN + 1}}
+
+    with open("day2_input.txt", mode="r") as input_file:
+        for line in input_file:
+            line = line.rstrip()
+            opponent, you = line.split(" ")
+
+            total_points += moves[opponent][you]
+
+    return total_points
 
 
 def main():
-    silver = read_file()
-    gold = read_file(gold=True)
-    print(f"Silver: {silver}\nGold: {gold}")
+    silver_ = silver()
+    gold_ = gold()
+    print(f"Silver: {silver_}\nGold: {gold_}")
 
 
 if __name__ == "__main__":
