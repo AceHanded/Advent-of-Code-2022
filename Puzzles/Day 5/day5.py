58b9a2cd5579deb21477aeb7d2c019068498f615
Vili Könönen <119615259+AceHanded@users.noreply.github.com>
2022-12-04 10:59:34 +0200

    Add files via upload

---
+++ Puzzles/Day 5/day5.py
@@ -0,0 +1,21 @@
+"""
+Day 5 puzzles of the Advent of Code.
+"""
+
+
+def silver():
+    ...
+
+
+def gold():
+    ...
+
+
+def main():
+    silver_ = silver()
+    gold_ = gold()
+    print(f"Silver: {silver_}\nGold: {gold_}")
+
+
+if __name__ == "__main__":
+    main()
