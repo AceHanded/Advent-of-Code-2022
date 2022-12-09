ae856ac9b0343aa6e5e767c99d4235bb051c5709
Vili Könönen <119615259+AceHanded@users.noreply.github.com>
2022-12-09 16:50:11 +0200

    Upload templates

---
+++ Puzzles/Day 11/day11.py
@@ -0,0 +1,23 @@
+"""
+Day 11 puzzles of the Advent of Code (11/25).
+"""
+
+
+def silver():
+    """The silver star solution."""
+    ...
+
+
+def gold():
+    """The gold star solution."""
+    ...
+
+
+def main():
+    silver_ = silver()
+    gold_ = gold()
+    print(f"Silver: {silver_}\nGold: {gold_}")
+
+
+if __name__ == "__main__":
+    main()
